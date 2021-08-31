f1e1aa6be9e2fdf83fb65219d122998d6fd7f4a5
Vitaly Fedyunin <vitaly.fedyunin@gmail.com>
2021-08-31 15:15:12 -0400

    [WIP] Example of DataPipes and DataFrames integration
    ghstack-source-id: d251aabe0201be838f034cf123240d24486cea26
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/60840

--- torch/utils/data/dataset.py
+++ torch/utils/data/dataset.py
@@ -24,6 +24,11 @@ from ... import Generator, Tensor
 T_co = TypeVar('T_co', covariant=True)
 T = TypeVar('T')
 
+UNTRACABLE_DATAFRAME_PIPES = ['batch',  # As it returns DataChunks
+                              'groupby',   # As it returns DataChunks
+                              'dataframes_as_tuples',  # As it unpacks DF
+                              'trace_as_dataframe',  # As it used to mark DF for tracing
+                              ]
 
 class DataChunk(list, Generic[T]):
     def __init__(self, items):
@@ -82,12 +87,26 @@ class Dataset(Generic[T_co]):
         cls.functions[function_name] = function
 
     @classmethod
-    def register_datapipe_as_function(cls, function_name, cls_to_register):
+    def register_datapipe_as_function(cls, function_name, cls_to_register, enable_df_api_tracing=False):
         if function_name in cls.functions:
+            print(cls.functions)
             raise Exception("Unable to add DataPipe function name {} as it is already taken".format(function_name))
 
         def class_function(cls, source_dp, *args, **kwargs):
-            return cls(source_dp, *args, **kwargs)
+            result_pipe = cls(source_dp, *args, **kwargs)
+            if isinstance(result_pipe, Dataset):
+                if enable_df_api_tracing or isinstance(source_dp, DFIterDataPipe) or getattr(result_pipe, '_dp_cast_to_df', False):
+                    if function_name not in UNTRACABLE_DATAFRAME_PIPES:
+                        result_pipe = result_pipe.trace_as_dataframe()
+
+                if getattr(result_pipe, '_dp_nesting_depth', None) is None:
+                    result_pipe._dp_nesting_depth = getattr(source_dp, '_dp_nesting_depth', None)
+
+                if getattr(result_pipe, '_dp_contains_dataframe', None) is None:
+                    result_pipe._dp_contains_dataframe = getattr(source_dp, '_dp_contains_dataframe', None)
+
+            return result_pipe
+
         function = functools.partial(class_function, cls_to_register)
         cls.functions[function_name] = function
 
@@ -227,6 +246,8 @@ class IterableDataset(Dataset[T_co], metaclass=_DataPipeMeta):
             raise Exception("Attempt to override existing reduce_ex_hook")
         IterableDataset.reduce_ex_hook = hook_fn
 
+class DFIterDataPipe(IterableDataset):
+    pass
 
 class TensorDataset(Dataset[Tuple[Tensor, ...]]):
     r"""Dataset wrapping tensors.
